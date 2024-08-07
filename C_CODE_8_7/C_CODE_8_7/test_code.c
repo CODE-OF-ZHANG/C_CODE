23bd010d3e85163b538b3a61bdcfed1cbd8fcde4
bow-elongation <3363967646@qq.com>
2024-08-07 16:13:35 +0800

    C语言作业代码提交

---
+++ C_CODE_8_7/C_CODE_8_7/test_code.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//    int a = 40, c = 212;
+//    int x = (-8 + 22) * a - 10 + c / 2;
+//    printf("%d\n", x);
+//    return 0;
+//}
---
+++ C_CODE_8_7/C_CODE_8_7/test_code02.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//    int iq;
+//    while (scanf("%d", &iq) != EOF) {
+//        if (iq >= 140)
+//        {
+//            printf("Genius");
+//        }
+//    }
+//    return 0;
+//}
