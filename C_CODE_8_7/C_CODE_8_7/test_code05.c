570f507e4c2d425dd33566bfc7ab106f2b5c12b2
bow-elongation <3363967646@qq.com>
2024-08-07 23:04:47 +0800

    C语言作业代码

---
+++ C_CODE_8_7/C_CODE_8_7/test_code05.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//    int x, y;
+//    scanf("%d", &x);
+//    if (x < 0) { y = 1; printf("%d\n", y); }
+//    else if (x == 0) { y = 0; printf("%d\n", y); }
+//    else { y = -1; printf("%d\n", y); }
+//    return 0;
+//}
---
+++ C_CODE_8_7/C_CODE_8_7/test_code06.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//    int a, b;
+//    scanf("%d%d", &a, &b);
+//    int x = a / b;
+//    int y = a % b;
+//    printf("%d %d\n", x, y);
+//    return 0;
+//}
