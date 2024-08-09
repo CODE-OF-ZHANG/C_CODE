ce4240b58b8e6cba7a8937d28baa524fc9053264
CODE_OF_ZHANG <3363967646@qq.com>
2024-08-09 22:10:52 +0800

    C语言作业代码

---
+++ C_CODE_8_9/C_CODE_8_9/test04.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+// 打印1000年到2000年之间的闰年
+
+//int main()
+//{
+//	for (int i = 1000; i <= 2000; i++)
+//	{
+//		if (((i % 4 == 0) && (i % 100 != 0)) || ((i % 400) == 0))
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
