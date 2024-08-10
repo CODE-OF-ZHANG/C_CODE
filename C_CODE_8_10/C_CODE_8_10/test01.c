081f4a013f0fcc57370cec317c200a605cc1d70f
CODE_OF_ZHANG <3363967646@qq.com>
2024-08-10 12:43:36 +0800

    C语言作业代码

---
+++ C_CODE_8_10/C_CODE_8_10/test01.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+// ¥Ú”°æ≈æ≈≥À∑®±Ì
+
+//int main()
+//{
+//	int i, j;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", j, i, j * i);
+//		}
+//		putchar('\n');
+//	}
+//}
