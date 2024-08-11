32f28dd255891f4ddd4e0d968a755efb107f7cbd
CODE_OF_ZHANG <3363967646@qq.com>
2024-08-11 18:47:57 +0800

    C语言作业代码

---
+++ C_CODE_8_11/C_CODE_8_11/test03.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//    int n = 0, min = 0;
+//    scanf("%d", &n);
+//    min = 4 * (n / 12) + 2;
+//    printf("%d\n", min);
+//    return 0;
+//}
