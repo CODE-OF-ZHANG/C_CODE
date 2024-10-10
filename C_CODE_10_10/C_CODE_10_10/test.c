fd3ca585718233b43a17ccd83e74d9b6a558058b
CODE_OF_ZHANG <3363967646@qq.com>
2024-10-10 23:40:48 +0800

    爱心代码

---
+++ C_CODE_10_10/C_CODE_10_10/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+	float a, x, y;
+	system(" color 0c");
+	for (y = 1.5f; y > -1.5f; y -= 0.1f)
+	{
+		for (x = -1.5f; x < 1.5f; x += 0.1f)
+		{
+			a = x * x + y * y - 1;
+			putchar(a * a * a - x * x * y * y * y < 0.0f ? '*' : ' ');
+		}
+		Sleep(5);
+		putchar('\n');
+	}
+	printf("hello world\n");
+	return 0;
+}
